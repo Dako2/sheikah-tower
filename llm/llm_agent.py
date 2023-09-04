259940f91e8e85e168fde230809f35e457636a7c
Ko Da <125230912+Dako2@users.noreply.github.com>
2023-09-04 12:39:35 -0700

    Add files via upload

---
+++ llm/llm_agent.py
@@ -0,0 +1,42 @@
+﻿import openai
+
+class Conversation:
+    def __init__(self):
+        self.messages = [{"role": "system", "content": "Be a helpful assitant and language interpreter for Monoco."},{"role": "assistant", "content": "Hi Link!"}]
+        #print("\n\n\
+    def call_api(self):
+        # text + user inputs as inputs of calling LLM APIs
+        response = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages=self.messages,
+            max_tokens=200,
+            temperature=0.4  # More parameters can be added if necessary
+            # presence_penalty=0, frequency_penalty=0 # [-2~2]
+        )
+
+        # Get response from calling the API
+        completion = response['choices']
+        message = completion[-1]['message']['content']
+        return message
+        # TODO: Add logic to calculate tokens used and speed
+   
+    def rolling_convo(self, user_input, found_db_texts, found_db_user_data):
+        self.messages.append({"role": "user", "content": user_input})
+        vec_info =''
+        if found_db_texts:
+            self.messages.append({"role": "system", "content": f"Some helpful knowledge: {found_db_texts}"})
+            vec_info = f'\n<p style="color: red;">found loc info: {found_db_texts}</p>'
+
+        if found_db_user_data:
+            self.messages.append({"role": "system", "content": f"Something you know about the user: {found_db_user_data}"})
+            vec_info = f'\n<p style="color: red;">found user info: {found_db_user_data}</p>'
+        #for message in self.messages:
+        #    print(message)
+        #return self.messages
+    
+        chat_response = self.call_api() # use new added user_input to call API again
+        print(f"\n\nVIP Guide: {chat_response}")
+        self.messages.append({"role": "assistant", "content": chat_response}) #['system', 'assistant', 'user', 'function']
+        print(type(chat_response),chat_response)
+        
+        return chat_response + vec_info
