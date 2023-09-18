32724d21edb1dbe3e8624f15398c106735c4dada
Ko Da <125230912+Dako2@users.noreply.github.com>
2023-09-17 21:58:22 -0700

    v0.0

--- wrapper.py
+++ wrapper.py
@@ -1,149 +1,102 @@
-import openai
-from environment import OPENAI_API_KEY
-from datetime import datetime
-openai.api_key = OPENAI_API_KEY
-from llm.llm_agent import Conversation
-#from mec_apis.location_manager import LocationManager
-from VecDB import VecDataBase
-from mec_apis.mec_location_api import fetch_user_coordinates, fetch_user_coordinates_zoneid_cellid, fetch_user_coordinates_zoneid_cellid_real
 import json
-import time
-import image_vecdb
-from mec_apis.mec_virutal_server import VirtualMEC
+from datetime import datetime
 
-# [skip if saved already] convert text in db into embeddings
-#text_to_ebds_csv('db/loc_apiexhibit-info.csv','db/exhibit-info-ebds.csv')
-#text_to_ebds_csv('db/user-data.csv','db/user-data-ebds.csv')
-DATA_PATH={'loc1':'db/exhibit-info.csv', 'user1':'db/user-data.csv'}
+import openai
+import image_vecdb
+from llm.llm_agent import Conversation
+from VecDB import VecDataBase
+from mec_apis.mec_location_api import (fetch_user_coordinates, 
+                                       fetch_user_coordinates_zoneid_cellid, 
+                                       fetch_user_coordinates_zoneid_cellid_real)
+from mec_apis.mec_virutal_server import VirtualMEC
+from environment import OPENAI_API_KEY
+
+openai.api_key = OPENAI_API_KEY
+
+DATA_PATH = {
+    'loc1': 'db/exhibit-info.csv',
+    'user1': 'db/user-data.csv'
+}
+
 FETCH_URL = 'https://try-mec.etsi.org/sbxkbwuvda/mep1/location/v2/queries/users?address='
+
 
 def load_jsonl(file_path):
     with open(file_path, 'r') as file:
         return [json.loads(line) for line in file]
-    
-class MECApp():
-    def __init__(self, user_IP_address = '10.100.0.1') -> None:
+
+
+class MECApp:
+    def __init__(self, user_IP_address='10.100.0.1') -> None:
         self.convo = Conversation()
-        #log_file_path = "db/user_event_log_file.json"
-        #db_location_file_path = "db/monaco_coordinates.json"
         self.ip_addr = user_IP_address
-        #self.locationManager = LocationManager(user_IP_address, log_file_path, db_location_file_path, FETCH_URL)
-        self.v = VecDataBase(db_csv_paths = DATA_PATH, update_db=True)
+        self.v = VecDataBase(db_csv_paths=DATA_PATH, update_db=True)
         self.mec_virtual = VirtualMEC()
         self.places_dict = {}
         with open('./db/monoco_zone_cellid_places.json', 'r') as file:
-            self.db_json = json.load(file) 
+            self.db_json = json.load(file)
         self.cellid = None
         self.zoneid = None
         self.image_db = image_vecdb.ImageVecDataBase('./db/images', './db/images/embeddings')
-        # self.image_db_jsonlist = load_jsonl("./db/images/metadata.jsonl")
-        
-    def analyze_image_api(self, filename = './upload/image.jpeg'):
-        # Read image
+
+    def analyze_image_api(self, filename='./upload/image.jpeg'):
         img = image_vecdb.Image.open(filename)
         try:
             most_similar_img, most_similar_img_idx, sim_score = self.image_db.search_db(img)
-            print("Score: %.4f" % (sim_score))
-            print("Index of most similar image in DB: %d" % (most_similar_img_idx))
-            # print(self.image_db_jsonlist[most_similar_img_idx])
-            #return sim_score, most_similar_img_idx
-            print(sim_score, self.image_db.db_image_info(most_similar_img_idx), self.image_db.db_image_prompt(most_similar_img_idx))
-            self.convo.messages = []
-            self.convo.messages.append({"role":"system","content":self.image_db.db_image_prompt(most_similar_img_idx)})
-            
+            self.convo.messages = [{
+                "role": "system",
+                "content": self.image_db.db_image_prompt(most_similar_img_idx)
+            }]
             return [sim_score, self.image_db.db_image_info(most_similar_img_idx), self.image_db.db_image_prompt(most_similar_img_idx)]
         except:
             return [None, None, None]
 
-        
     def loc_user_places_api(self):
-        
-        #latitude, longitude, _, self.cellid, self.zoneid = fetch_user_coordinates_zoneid_cellid_real()
-        #print(latitude, longitude, _, self.cellid, self.zoneid, "\n\n--")
         latitude, longitude, _, self.cellid, self.zoneid = self.mec_virtual.fetch_user_coordinates_zoneid_cellid()
-        print(latitude, longitude, _, self.cellid, self.zoneid)
-
-        # todo to delete below
-        #self.cellid, self.zoneid = "4g-macro-cell-4", "zone02"
-        print("\n\n************",latitude, longitude, _, self.cellid, self.zoneid)
         try:
             self.places_dict = self.db_json[self.zoneid][self.cellid]["places"]
             place_names = ', '.join(map(str, self.places_dict.keys()))
-            
             self.convo.messages[0]["content"] = f"be a helpful local guide at {place_names}. Respond concisely and cheerfully."
-            
-            print(f"\n\n{self.convo.messages}\n\n")
-            return (latitude, longitude), self.places_dict #(latitude, longitude) Egyption Museum: 43.731724, 7.423574
+            return (latitude, longitude), self.places_dict
         except:
             return (None, None), {}
-        
+
     def chat_api(self, user_input):
         loc1_found_db_texts = ""
-        #self.loc_user_places_api()
-        #time.sleep(1)
         if self.places_dict:
-            for loc_name, value in self.places_dict.items(): #loc_name, loc_dictionary: lat, long, db_path
+            for loc_name, value in self.places_dict.items():
                 try:
-                    print(f"searching{loc_name}:{value}")
-                    text, loc1_found_score = self.v.search_db(user_input, value['db_path'])
+                    text, _ = self.v.search_db(user_input, value['db_path'])
                     loc1_found_db_texts += text
-                    print(f"Found loc info {loc_name}: {text}")
                 except:
-                    print("bypassing..........\n\n\n")
-        user_found_db_texts, user_found_score = self.v.search_db(user_input, DATA_PATH['user1'])
-
+                    pass
+        user_found_db_texts, _ = self.v.search_db(user_input, DATA_PATH['user1'])
         output = self.convo.rolling_convo(user_input, loc1_found_db_texts, user_found_db_texts)
-        print("\n\n\n===============", self.convo.messages, "==============================\n\n\n")
-
         return output
-    
-    """
-    def loc_api(self, radius = 1000):
-        nearby_locations = None
-        event = self.locationManager.fetch_nearby_locations(radius)
-        user_live_coor = f"User now at: {event['user_live_coor']}"
 
-        if event[list(event.keys())[2]]:
-            self.nearby_locations = f"{list(event.keys())[2]}: {event[list(event.keys())[2]]}" # nearby_locations within 500: ['Monte Carlo Casino', 'Japanese Garden in Monaco']
-        return user_live_coor, self.nearby_locations, event
-    """
-    
-    def loc_user_api(self,):
+    def loc_user_api(self):
         try:
-            user_live_coor = fetch_user_coordinates()
-            return user_live_coor
+            return fetch_user_coordinates()
         except:
             print("user loc cannot be found")
             return (None, None)
-    
-    def desert_mode(self, user_input): # use user db, city coordinates & user location (outside this function)
-        #loc1_found_db_texts, loc1_found_score = self.v.search_db(user_input, DATA_PATH['loc1'])
-        user_found_db_texts, user_found_score = self.v.search_db(user_input, DATA_PATH['user1'])
-        return user_found_db_texts, user_found_score # output tuple ('', [], '', [])
 
-    def spot_mode(self, user_input): # use user db, spot db (e.g. museum), no user location (already entered the place)
+    def desert_mode(self, user_input):
+        user_found_db_texts, user_found_score = self.v.search_db(user_input, DATA_PATH['user1'])
+        return user_found_db_texts, user_found_score
+
+    def spot_mode(self, user_input):
         loc1_found_db_texts, loc1_found_score = self.v.search_db(user_input, DATA_PATH['loc1'])
         user_found_db_texts, user_found_score = self.v.search_db(user_input, DATA_PATH['user1'])
-        return loc1_found_db_texts, loc1_found_score, user_found_db_texts, user_found_score # output tuple ('', [], '', [])
+        return loc1_found_db_texts, loc1_found_score, user_found_db_texts, user_found_score
 
     def if_enter_a_spot(self):
-        confirmation = True
-        positive_confirmations = ["yes", "ok", "sure", "of course", "let's do it", "fine", "yeah", "yup"]
-        #if self.
-        # ()[1]:
-            #nearby_locations = self.loc_api()[1]
-            #user_input = input(f"Are you interested in exploring these nearby spots further with me? {nearby_locations}").lower()
-            #if any(word in user_input for word in positive_confirmations):
-            #    confirmation = True
-        return confirmation
-    
+        return True  # TODO: Implement further logic
+
     def if_leave_a_spot(self):
-        return False # TODO   
+        return False  # TODO: Implement further logic
+
 
 if __name__ == "__main__":
     mec = MECApp('10.100.0.1')
-    
     mec.loc_user_places_api()
-    print('\n\n\n\n')
-    #mec.chat_api("hello, any interesting spots? ")
-    #mec.analyze_image_api('./test_data/images/test_0001.jpeg')
