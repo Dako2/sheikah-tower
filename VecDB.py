b39bfdb21afeb55c7e4133b27c685bde550d6afe
Sharu <jsrshark110@gmail.com>
2023-10-14 14:36:09 -0700

    Minor renames

--- VecDB.py
+++ VecDB.py
@@ -78,10 +78,11 @@ class VecDataBase():
         return db_json_file + '.ebd'
 
     def search_db(self, user_input, db_json_file, threshold=0.5, top_n = 2): #todo
-        if self.get_embed_path(db_json_file) not in list(self.cache_vector_database.keys()): #quick load corpus_json
+        db_ebd_file = self.get_embed_path(db_json_file)
+        if db_ebd_file not in list(self.cache_vector_database.keys()): #quick load corpus_json
             self.load_db([db_json_file])
 
-        corpus_ebd = self.cache_vector_database[self.get_embed_path(db_json_file)]
+        corpus_ebd = self.cache_vector_database[db_ebd_file]
         query_embedding = self.model.encode(user_input, convert_to_numpy=True) #user input -> query_embedding
         cosine_scores = util.pytorch_cos_sim(query_embedding, list(corpus_ebd.values()))      
         
