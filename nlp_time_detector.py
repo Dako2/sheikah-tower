eb53eafd292792848360e261f0897c16d14437e3
Sharu <jsrshark110@gmail.com>
2023-10-14 22:19:59 -0700

    Update nlp time extractor

--- nlp_time_detector.py
+++ nlp_time_detector.py
@@ -5,10 +5,10 @@ from dateparser.search import search_dates
 
 def extract_time(sentence):
     sentence_dates = search_dates(sentence)
-    # Dedup
-    sentence_dates = list(dict.fromkeys(sentence_dates))
-    print(sentence_dates)
-    return sentence_dates
+    if sentence_dates is None:
+        return None
+    
+    return sentence_dates[0]
 
 
 if __name__ == "__main__":
@@ -16,3 +16,4 @@ if __name__ == "__main__":
     print(extract_time("What are the events in 1 hour?"))
     print(extract_time("What were the events at 2pm today?"))
     print(extract_time("What were the events at 2pm tmr?"))
+    print(extract_time("Who is David?"))
