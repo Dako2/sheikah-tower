75b6f380aa89bab42e23e25514ee75ff89ebfffe
Ko Da <125230912+Dako2@users.noreply.github.com>
2023-10-15 14:04:03 -0700

    123

--- wrapper.py
+++ wrapper.py
@@ -22,7 +22,7 @@ DATA_PATH = {
 FETCH_URL = "https://try-mec.etsi.org/sbxkbwuvda/mep1/location/v2/queries/users?address="
 DEFAULT_PROMPT = "Respond friendly, cheerfully and concisely within 50 words. Keep the conversation's flow by politely asking short question or for clarification or additional details when unsure."
 DEFAULT_PROMPT_PHOTO = "The user took a photo." #the user input prompt in image case
-UPDATE_VectDataBase = False
+UPDATE_VectDataBase = True
 
 
 def load_jsonl(file_path):
