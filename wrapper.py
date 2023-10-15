7c3520ec5411f78dd3da25108502adc37ef51f7b
Ko Da <125230912+Dako2@users.noreply.github.com>
2023-10-15 14:17:47 -0700

    photo prompt

--- wrapper.py
+++ wrapper.py
@@ -21,7 +21,7 @@ DATA_PATH = {
 
 FETCH_URL = "https://try-mec.etsi.org/sbxkbwuvda/mep1/location/v2/queries/users?address="
 DEFAULT_PROMPT = "Respond friendly, cheerfully and concisely within 50 words. Keep the conversation's flow by politely asking short question or for clarification or additional details when unsure."
-DEFAULT_PROMPT_PHOTO = "The user took a photo." #the user input prompt in image case
+DEFAULT_PROMPT_PHOTO = "The user took a photo. Please help describe the photos based on the detailed information." #the user input prompt in image case
 UPDATE_VectDataBase = True
 
 
