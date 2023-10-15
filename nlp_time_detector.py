186b8f7c7cafdbe8af99d17579530844f0fc2673
Sharu <jsrshark110@gmail.com>
2023-10-14 22:54:17 -0700

    Merge time search in normal text search

--- nlp_time_detector.py
+++ nlp_time_detector.py
@@ -8,7 +8,7 @@ def extract_time(sentence):
     if sentence_dates is None:
         return None
     
-    return sentence_dates[0]
+    return sentence_dates
 
 
 if __name__ == "__main__":
@@ -16,4 +16,5 @@ if __name__ == "__main__":
     print(extract_time("What are the events in 1 hour?"))
     print(extract_time("What were the events at 2pm today?"))
     print(extract_time("What were the events at 2pm tmr?"))
+    print(extract_time("What are events about Sonic at 1pm Oct 17"))
     print(extract_time("Who is David?"))
